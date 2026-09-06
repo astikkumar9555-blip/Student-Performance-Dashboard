58abe669db510bc9522511b416afc3383f3f73cb
Ashutosh Kumar <astikkumar9555@gmail.com>
2026-09-06 18:33:19 +0530

    few lines of javascript

---
+++ script.js
@@ -0,0 +1,32 @@
+ const student=[
+    {
+        name:"Ashutosh Kumar",
+        rollNo:101,
+        marks:{
+            Maths:80,
+            Physics:75,
+            Javascript:90
+        }
+    },
+    {
+        name:"Rahul",
+        rollNo:102,
+
+        marks:{
+            Maths:70,
+            Physics:65,
+            Javascript:85
+        }
+
+    },
+    {
+        name:"Aman",
+        rollNo:103,
+        marks:{
+             Maths:92,
+            Physics:88,
+            Javascript:95
+        }
+    }
+ ];
+ 
